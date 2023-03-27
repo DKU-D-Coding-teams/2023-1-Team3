00eaec1fdec048f4c9557e8c6c582a3f3ffc1e39
HongdeokKim <qchdkim@naver.com>
2023-03-27 16:20:24 +0900

    feat: SMTP 적용, MailUtil 구현

---
+++ VISTA/src/main/java/DKUDCoding20231Team3/VISTA/dto/request/MailRequest.java
@@ -0,0 +1,16 @@
+package DKUDCoding20231Team3.VISTA.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Getter
+@NonNull
+@AllArgsConstructor
+@NoArgsConstructor
+public class MailRequest {
+
+    private String mail;
+
+}
---
+++ VISTA/src/main/java/DKUDCoding20231Team3/VISTA/util/MailUtil.java
@@ -0,0 +1,33 @@
+package DKUDCoding20231Team3.VISTA.util;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MailUtil {
+
+    private final JavaMailSender javaMailSender;
+
+    public String codeSend(String mail) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        String code = Integer.toString((int)(Math.random() * (999999 - 100000 + 1) + 100000));
+
+        message.setTo(mail);
+        message.setSubject("[VISTA] 메일 인증코드 전송");
+        message.setText(
+                "VISTA에서 회원 인증을 위한 코드를 전송했습니다.\n" +
+                        "하단의 코드를 입력해주세요\n\n" +
+                        "--\n" +
+                        code +
+                        "\n"
+        );
+        javaMailSender.send(message);
+
+        return code;
+    }
+
+}
