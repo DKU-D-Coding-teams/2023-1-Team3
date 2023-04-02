6af28ed9edf76ab4fad4441dd286a7ae779632f9
HongdeokKim <qchdkim@naver.com>
2023-04-02 23:38:54 +0900

    fix: controller 함수, 경로 용어 수정

--- VISTA/src/main/java/DKUDCoding20231Team3/VISTA/controller/MemberController.java
+++ VISTA/src/main/java/DKUDCoding20231Team3/VISTA/controller/MemberController.java
@@ -1,7 +1,6 @@
 package DKUDCoding20231Team3.VISTA.controller;
 
 import DKUDCoding20231Team3.VISTA.dto.request.*;
-import DKUDCoding20231Team3.VISTA.dto.response.MemberResponse;
 import DKUDCoding20231Team3.VISTA.dto.response.SignInResponse;
 import DKUDCoding20231Team3.VISTA.dto.response.SignUpResponse;
 import DKUDCoding20231Team3.VISTA.service.MemberService;
@@ -18,29 +17,6 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("create")
-    public ResponseEntity<MemberResponse> create(@RequestBody MemberRequest memberRequest) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(memberService.create(memberRequest));
-    }
-
-    @GetMapping("read/{memberId}")
-    public ResponseEntity<MemberResponse> read(@PathVariable Long memberId) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(memberService.read(memberId));
-    }
-
-    @PostMapping("update/{memberId}")
-    public ResponseEntity<MemberResponse> update(@PathVariable Long memberId, @RequestBody MemberRequest memberRequest) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(memberService.update(memberId, memberRequest));
-    }
-
-    @DeleteMapping("delete")
-    public ResponseEntity<HttpStatus> delete(@RequestParam Long memberId) {
-        return ResponseEntity.status(memberService.delete(memberId)).build();
-    }
-
     @PostMapping("mail")
     public ResponseEntity<HttpStatus> sendMail(@Valid @RequestBody MailRequest mailRequest) {
         return ResponseEntity.status(memberService.sendMail(mailRequest)).build();
@@ -51,12 +27,12 @@ public class MemberController {
         return ResponseEntity.status(memberService.checkMail(mailCodeRequest)).build();
     }
 
-    @PostMapping("signUp")
-    public ResponseEntity<SignUpResponse> signup(@Valid @RequestBody SignUpRequest signUpRequest) {
+    @PostMapping("signup")
+    public ResponseEntity<SignUpResponse> signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.signUp(signUpRequest));
     }
 
-    @PostMapping("signIn")
+    @PostMapping("signin")
     public ResponseEntity<SignInResponse> signIn(@Valid @RequestBody SignInRequest signInRequest) {
         return ResponseEntity.status(HttpStatus.OK).body(memberService.signIn(signInRequest));
     }
