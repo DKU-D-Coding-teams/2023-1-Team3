99253cd92b8870f47d4949c45383367a8d4a2691
HongdeokKim <qchdkim@naver.com>
2023-04-12 04:07:21 +0900

    feat: member/suggest API 구현(강호) 및 수정

--- VISTA/src/main/java/DKUDCoding20231Team3/VISTA/dto/response/SuggestResponse.java
+++ VISTA/src/main/java/DKUDCoding20231Team3/VISTA/dto/response/SuggestResponse.java
@@ -1,6 +1,5 @@
 package DKUDCoding20231Team3.VISTA.dto.response;
 
-import DKUDCoding20231Team3.VISTA.domain.entity.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,14 +8,16 @@ import java.util.List;
 
 @Getter
 @Builder
-//@NonNull
 @AllArgsConstructor
 public class SuggestResponse {
 
+    private boolean endPageSignal;
+
     private List<MemberResponse> memberResponses;
 
-    public static SuggestResponse of(List<MemberResponse> memberResponses) {
+    public static SuggestResponse of(boolean endPageSignal, List<MemberResponse> memberResponses) {
         return SuggestResponse.builder()
+                .endPageSignal(endPageSignal)
                 .memberResponses(memberResponses)
                 .build();
     }
--- VISTA/src/main/java/DKUDCoding20231Team3/VISTA/service/MemberService.java
+++ VISTA/src/main/java/DKUDCoding20231Team3/VISTA/service/MemberService.java
@@ -2,9 +2,9 @@ package DKUDCoding20231Team3.VISTA.service;
 
 import DKUDCoding20231Team3.VISTA.domain.entity.Member;
 import DKUDCoding20231Team3.VISTA.domain.entity.MemberLog;
-import DKUDCoding20231Team3.VISTA.domain.enumerations.Gender;
 import DKUDCoding20231Team3.VISTA.domain.repository.MemberLogRepository;
 import DKUDCoding20231Team3.VISTA.domain.repository.MemberRepository;
+import DKUDCoding20231Team3.VISTA.dto.database.MemberListInterface;
 import DKUDCoding20231Team3.VISTA.dto.request.*;
 import DKUDCoding20231Team3.VISTA.dto.response.*;
 import DKUDCoding20231Team3.VISTA.exception.VistaException;
@@ -18,9 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
@@ -31,58 +29,11 @@ import static DKUDCoding20231Team3.VISTA.exception.ErrorCode.*;
 public class MemberService {
 
     private final MemberRepository memberRepository;
-
     private final MemberLogRepository memberLogRepository;
-
     private final MailUtil mailUtil;
-
     private final RedisUtil redisUtil;
-
     private final JwtTokenProvider jwtTokenProvider;
-
     private final PasswordEncoder passwordEncoder;
-
-//    public MemberResponse create(MemberRequest memberRequest) {
-//        final Member member = Member.of(memberRequest);
-//        memberRepository.save(member);
-//
-//        return MemberResponse.of(member);
-//    }
-//
-//    public DummyMembersResponse DummyMembers() {
-//        Member[] memberList = new Member[10];
-//
-//        for (int i=0; i<10; i++) {
-//            memberList[i] = Member.of(
-//                String.valueOf(i+1),
-//                String.valueOf(i+1),
-//                String.valueOf(i+1),
-//                Gender.MALE,
-//                LocalDate.now()
-//            );
-//            memberRepository.save(memberList[i]);
-//        }
-//
-//        return DummyMembersResponse.of(memberList);
-//    }
-//
-//    public MemberResponse read(Long memberId) {
-//        return MemberResponse.of(memberRepository.findById(memberId).orElseThrow());
-//    }
-//
-//    public MemberResponse update(Long memberId, MemberRequest memberRequest) {
-//        Member member = memberRepository.findById(memberId).orElseThrow();
-//        member.updateMember(memberRequest);
-//        memberRepository.save(member);
-//
-//        return MemberResponse.of(member);
-//    }
-
-    public HttpStatus delete(Long memberId) {
-        memberRepository.deleteById(memberId);
-
-        return HttpStatus.NO_CONTENT;
-    }
 
     public HttpStatus sendMail(MailRequest mailRequest) {
         if (memberRepository.existsByMail(mailRequest.getMail())) throw new VistaException(ALREADY_SAVED_MEMBER);
@@ -131,40 +82,34 @@ public class MemberService {
     }
 
     public SuggestResponse suggest(HttpServletRequest httpServletRequest) {
-//        Member requestedMember = findMemberByHttpServlet(httpServletRequest);
+        final int SUGGEST_SIZE = 5;
 
-        long max = memberRepository.count();
+        final Member member = findMemberByHttpServlet(httpServletRequest);
+        List<MemberListInterface> suggestMembers = memberRepository.getSuggestQuery(member.getMemberId());
+
+        boolean endPageSignal = false;
+        Random random = new Random();
         List<MemberResponse> memberResponses = new ArrayList<>();
+        for(int i = 0; i < SUGGEST_SIZE; i++) {
+            if (suggestMembers.size() == 0) {
+                memberLogRepository.deleteByFromId(member.getMemberId());
+                endPageSignal = true;
+                break;
+            }
+            int randomIndex = random.nextInt(suggestMembers.size());
+            MemberListInterface randomMember = suggestMembers.get(randomIndex);
+            suggestMembers.remove(randomMember);
 
-        List temp = memberRepository.temp(1L);
-
-        for (int i=0; i<5; i++) {
-            Random random = new Random();
-            int randomIndex = random.nextInt(temp.size());
-            Object[] randomMember = (Object[]) temp.get(randomIndex);
-            temp.remove(randomMember);
-//            memberLogRepository.saveFromIdAndToId(1L, (Long)randomMember[0]);
-
-            System.out.println("MemberID: " + randomMember[0]);
-            System.out.println("Name: " + randomMember[1]);
-            System.out.println("Gender: " + randomMember[2]);
-            System.out.println("Birth: " + randomMember[3]);
-
-            MemberResponse memberResponse = MemberResponse.of((Long)randomMember[0], (String)randomMember[1], (Gender)randomMember[2], (LocalDate)randomMember[3]);
-            memberResponses.add(memberResponse);
+            memberLogRepository.save(MemberLog.of(member.getMemberId(), randomMember.getMemberId(), false));
+            memberResponses.add(MemberResponse.of(
+                    randomMember.getMemberId(),
+                    randomMember.getName(),
+                    randomMember.getGender(),
+                    randomMember.getBirth()
+            ));
         }
 
-        /*
-            1. A는 모든 유저 아이디 리스트
-            2. A에서 내 아이디를 제외
-            3. A에서 내가 이전에 봤던 사람들의 아이디를 제외
-            4. A에서 5개의 아이디 랜덤 추출, 새로운 아이디 리스트 B에 저장
-            5. B에 담긴 아이디를 이용하여 멤버 테이블에서 멤버 정보 추출
-            6. 반환
-         */
-
-        return SuggestResponse.of(memberResponses);
-
+        return SuggestResponse.of(endPageSignal, memberResponses);
     }
 
     private Member findMemberByHttpServlet(HttpServletRequest httpServletRequest) {
@@ -183,43 +128,4 @@ public class MemberService {
         }
     }
 
-    public HttpStatus makeTestCase() {
-        List<Member> members = new ArrayList<>();
-
-        for(int i = 0; i < 30; i++) {
-            members.add(
-                    Member.builder()
-                            .mail(String.format("%08d@dankook.ac.kr", i))
-                            .password("1q2w3e4r1!")
-                            .name(String.format("%08dHGD", i))
-                            .gender((i%2 != 0) ? Gender.FEMALE : Gender.MALE)
-                            .birth(LocalDate.now())
-                            .build()
-            );
-        }
-        memberRepository.saveAll(members);
-
-        List<MemberLog> memberLogs = new ArrayList<>();
-
-        for(int i = 0; i < 5; i++) {
-            memberLogs.add(
-                    MemberLog.builder()
-                            .fromId(1L)
-                            .toId((long) ((i+1)*2))
-                            .signal((i%2 != 0) ? true : false)
-                            .build()
-            );
-        }
-        memberLogRepository.saveAll(memberLogs);
-
-        return HttpStatus.CREATED;
-
-    }
-
 }
-
-
-/*
-    /member/suggest
-
- */
