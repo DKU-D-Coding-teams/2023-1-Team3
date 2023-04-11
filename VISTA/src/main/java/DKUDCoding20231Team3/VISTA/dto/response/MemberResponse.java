9383e8b9edbcd14e9840567b2573c5ad40fbf50b
HongdeokKim <qchdkim@naver.com>
2023-04-12 02:20:36 +0900

    feat: likeResponse dto 구현

---
+++ VISTA/src/main/java/DKUDCoding20231Team3/VISTA/dto/response/MemberResponse.java
@@ -0,0 +1,35 @@
+package DKUDCoding20231Team3.VISTA.dto.response;
+
+import DKUDCoding20231Team3.VISTA.domain.enumerations.Gender;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class MemberResponse {
+
+    private Long memberId;
+
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private LocalDate birth;
+
+    public static MemberResponse of(Long memberId, String name, Gender gender, LocalDate birth) {
+        return MemberResponse.builder()
+                .memberId(memberId)
+                .name(name)
+                .gender(gender)
+                .birth(birth)
+                .build();
+    }
+
+}
