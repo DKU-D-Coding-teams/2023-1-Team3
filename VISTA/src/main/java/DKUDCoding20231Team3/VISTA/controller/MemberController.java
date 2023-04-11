1f07c6aab1a03ad289109a54b97ad50c40758030
HongdeokKim <qchdkim@naver.com>
2023-04-12 03:07:56 +0900

    fix: member/suggest API put->get으로 변경

--- VISTA/src/main/java/DKUDCoding20231Team3/VISTA/controller/MemberController.java
+++ VISTA/src/main/java/DKUDCoding20231Team3/VISTA/controller/MemberController.java
@@ -17,34 +17,6 @@ public class MemberController {
 
     private final MemberService memberService;
 
-//    @PostMapping("create")
-//    public ResponseEntity<MemberResponse> create(@RequestBody MemberRequest memberRequest) {
-//        return ResponseEntity.status(HttpStatus.CREATED)
-//                .body(memberService.create(memberRequest));
-//    }
-//
-//    @GetMapping("createdummymembers")
-//    public ResponseEntity<DummyMembersResponse> create() {
-//        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.DummyMembers());
-//    }
-//
-//    @GetMapping("read/{memberId}")
-//    public ResponseEntity<MemberResponse> read(@PathVariable Long memberId) {
-//        return ResponseEntity.status(HttpStatus.OK)
-//                .body(memberService.read(memberId));
-//    }
-//
-//    @PostMapping("update/{memberId}")
-//    public ResponseEntity<MemberResponse> update(@PathVariable Long memberId, @RequestBody MemberRequest memberRequest) {
-//        return ResponseEntity.status(HttpStatus.OK)
-//                .body(memberService.update(memberId, memberRequest));
-//    }
-//
-//    @DeleteMapping("delete")
-//    public ResponseEntity<HttpStatus> delete(@RequestParam Long memberId) {
-//        return ResponseEntity.status(memberService.delete(memberId)).build();
-//    }
-
     @PostMapping("mail")
     public ResponseEntity<HttpStatus> sendMail(@Valid @RequestBody MailRequest mailRequest) {
         return ResponseEntity.status(memberService.sendMail(mailRequest)).build();
@@ -65,21 +37,9 @@ public class MemberController {
         return ResponseEntity.status(HttpStatus.OK).body(memberService.signIn(signInRequest));
     }
 
-    @PutMapping("suggest")
+    @GetMapping("suggest")
     public ResponseEntity<SuggestResponse> suggest(HttpServletRequest httpServletRequest) {
         return ResponseEntity.status(HttpStatus.OK).body(memberService.suggest(httpServletRequest));
-    }
-
-//    @PutMapping("choice")
-//    public ResponseEntity<Integer> choiceLike(@RequestParam("id") Long memberId,
-//                                              @RequestParam("sign") Boolean sign,
-//                                              HttpServletRequest httpServletRequest) {
-//        return ResponseEntity.status(memberService.choiceLike(memberId, sign, httpServletRequest)).build();
-//    }
-
-    @GetMapping("makeTestCase")
-    public HttpStatus makeTestCase() {
-        return memberService.makeTestCase();
     }
 
 }
