14d67162a5aeb3f957a780fdf3bc8f61f5a8e7a6
Kangho <cialim0502@gamil.com>
2023-07-09 15:59:52 +0900

    23년 07월 09일 16시 00분
    수정 중
    - MemberController.java

--- VISTA/src/main/java/DKUDCoding20231Team3/VISTA/controller/MemberController.java
+++ VISTA/src/main/java/DKUDCoding20231Team3/VISTA/controller/MemberController.java
@@ -5,14 +5,12 @@ import DKUDCoding20231Team3.VISTA.dto.response.*;
 import DKUDCoding20231Team3.VISTA.exception.ErrorCode;
 import DKUDCoding20231Team3.VISTA.exception.VistaException;
 import DKUDCoding20231Team3.VISTA.service.MemberService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.tool.schema.spi.ExceptionHandler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -20,78 +18,111 @@ import java.security.NoSuchAlgorithmException;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("member/")
+@RequestMapping("/member")
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("mail")
+
+    // without token
+
+    @PostMapping("/mail")
     public ResponseEntity<HttpStatus> sendMail(@Valid @RequestBody MailRequest mailRequest) {
-        return ResponseEntity.status(memberService.sendMail(mailRequest)).build();
+        String memberMail = mailRequest.getMail();
+
+        return ResponseEntity.status(memberService.sendMail(memberMail)).build();
     }
 
-    @PostMapping("code")
+    @PostMapping("/code")
     public ResponseEntity<HttpStatus> checkMail(@Valid @RequestBody MailCodeRequest mailCodeRequest) {
-        return ResponseEntity.status(memberService.checkMail(mailCodeRequest)).build();
+        String memberMail = mailCodeRequest.getMail();
+        String memebrCode = mailCodeRequest.getCode();
+
+        return ResponseEntity.status(memberService.checkMail(memberMail, memebrCode)).build();
     }
 
-    @PostMapping("signup")
+    @PostMapping("/signup")
     public ResponseEntity<SignUpResponse> signup(@Valid @RequestBody SignUpRequest signUpRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(memberService.signUp(signUpRequest));
     }
 
-    @PostMapping("signin")
+    @PostMapping("/signin")
     public ResponseEntity<SignInResponse> signIn(@Valid @RequestBody SignInRequest signInRequest) {
-        return ResponseEntity.status(HttpStatus.OK).body(memberService.signIn(signInRequest));
+        String memberMail = signInRequest.getMail();
+        String memberPassword = signInRequest.getPassword();
+
+        return ResponseEntity.status(HttpStatus.OK).body(memberService.signIn(memberMail, memberPassword));
     }
 
-    @GetMapping("suggest")
-    public ResponseEntity<SuggestResponse> suggest(HttpServletRequest httpServletRequest) {
-        return ResponseEntity.status(HttpStatus.OK).body(memberService.suggest(httpServletRequest));
+
+    // with token
+
+    @GetMapping("/suggest")
+    public ResponseEntity<SuggestResponse> suggest() {
+        String memberMail = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return ResponseEntity.status(HttpStatus.OK).body(memberService.suggest(memberMail));
     }
 
-    @PutMapping("choice")
+    @PutMapping("/choice")
     public ResponseEntity<HttpStatus> choiceLike(@RequestParam("toId") Long toId,
-                                                 @RequestParam("likeSignal") Boolean likeSignal,
-                                                 HttpServletRequest httpServletRequest) {
-        return ResponseEntity.status(memberService.choiceLike(toId, likeSignal, httpServletRequest)).build();
+                                                 @RequestParam("likeSignal") Boolean likeSignal) {
+        String memberMail = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return ResponseEntity.status(memberService.choiceLike(toId, likeSignal, memberMail)).build();
     }
 
-    @PutMapping("block")
+    @PutMapping("/block")
     public ResponseEntity<HttpStatus> choiceBlock(@RequestParam("toId") Long toId,
-                                                 @RequestParam("blockSignal") Boolean blockSignal,
-                                                 HttpServletRequest httpServletRequest) {
-        return ResponseEntity.status(memberService.choiceBlock(toId, blockSignal, httpServletRequest)).build();
+                                                  @RequestParam("blockSignal") Boolean blockSignal) {
+        String memberMail = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return ResponseEntity.status(memberService.choiceBlock(toId, blockSignal, memberMail)).build();
     }
 
-    @GetMapping("likes")
-    public ResponseEntity<LikeResponse> getLikes(@RequestParam("page") Integer page,
-                                                 HttpServletRequest httpServletRequest) {
-        return ResponseEntity.status(HttpStatus.OK).body(memberService.getLikes(page, httpServletRequest));
+    @GetMapping("/likes")
+    public ResponseEntity<LikeResponse> getLikes(@RequestParam("page") Integer page) {
+        String memberMail = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return ResponseEntity.status(HttpStatus.OK).body(memberService.getLikes(page, memberMail));
     }
 
-    @PostMapping("reset")
-    public ResponseEntity<HttpStatus> resetPassword(@Valid @RequestBody ResetPasswordRequest resetPasswordRequest,
-                                                    HttpServletRequest httpServletRequest) {
-        return ResponseEntity.status(memberService.resetPassword(resetPasswordRequest, httpServletRequest)).build();
+    @PostMapping("/reset")
+    public ResponseEntity<HttpStatus> resetPassword(@Valid @RequestBody ResetPasswordRequest resetPasswordRequest) {
+        String memberMail = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return ResponseEntity.status(memberService.resetPassword(resetPasswordRequest, memberMail)).build();
     }
 
-    @PostMapping("images")
-    public ResponseEntity<MemberResponse> uploadImage(@RequestParam("image") MultipartFile image,
-                                                      HttpServletRequest httpServletRequest) throws IOException, NoSuchAlgorithmException {
+    @PostMapping("/images")
+    public ResponseEntity<MemberResponse> uploadImage(@RequestParam("image") MultipartFile image)
+            throws IOException, NoSuchAlgorithmException {
         if(image.getSize() == 0) throw new VistaException(ErrorCode.INVALID_MULTIPART_REQUEST);
-        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.uploadImage(image, httpServletRequest));
+        String memberMail = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return ResponseEntity.status(HttpStatus.CREATED).body(memberService.uploadImage(image, memberMail));
     }
 
-    @GetMapping("mypages")
-    public ResponseEntity<MemberResponse> getMyPages(HttpServletRequest httpServletRequest) {
-        return ResponseEntity.status(HttpStatus.OK).body(memberService.getMyPages(httpServletRequest));
+    @GetMapping("/mypages")
+    public ResponseEntity<MemberResponse> getMyPages() {
+        String memberMail = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return ResponseEntity.status(HttpStatus.OK).body(memberService.getMyPages(memberMail));
     }
 
-    @PostMapping("mypages")
-    public ResponseEntity<MemberResponse> updateMyPages(@Valid @RequestBody MyPageRequest myPageRequest,
-                                                      HttpServletRequest httpServletRequest) {
-        return ResponseEntity.status(HttpStatus.OK).body(memberService.updateMyPages(myPageRequest, httpServletRequest));
+    @PostMapping("/mypages")
+    public ResponseEntity<MemberResponse> updateMyPages(@Valid @RequestBody MyPageRequest myPageRequest) {
+        String memberMail = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return ResponseEntity.status(HttpStatus.OK).body(memberService.updateMyPages(myPageRequest, memberMail));
+    }
+
+    @PostMapping("/signout")
+    public ResponseEntity<SignOutResponse> signOut(@Valid @RequestBody SignOutRequest signOutRequest) {
+        System.out.println("MemberController method signOut - checkpoint 1");
+        String memberMail = signOutRequest.getMail();
+        String memberPassword = signOutRequest.getPassword();
+
+        return ResponseEntity.status(HttpStatus.OK).body(memberService.signOut(memberMail, memberPassword));
     }
 
 }
